1fc83a998aebf2810d42e8de200fe114e8f6781f
JulianTorresMendioroz <juliantorresmendioroz348@gmail.com>
2022-11-16 18:04:01 -0300

    agrego archivos en proceso de 3er entrega

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const CART = [];
 let total = 0;
 
-console.log('Cart:',CART)
+console.log('CART:',CART)
 
 function newUserOn() { 
 
@@ -17,158 +17,244 @@ if((newUser == '') || (newUser == ' ')) {
 
 }}
 
-newUserOn();
+//newUserOn();
 
-class Product {
-
-    constructor(id, name, price, stock){
-        
-        this.id = id
-        this.name = name
-        this.price = price
-        this.stock = stock
+const products = [
     
-    }
-
-}
-
-const product1 = new Product (1,'Buzo Verde', 3200, 2)
-const product2 = new Product (2,'Buzo Rojo', 4000, 2)
-const product3 = new Product (3,'Buzo Azul', 3500, 6)
-const product4 = new Product (4,'Buzo Blanco', 5000, 0)
-
-const PRODUCTS = [product1,product2,product3,product4];
+{
+    id: 1,
+    name: 'Buzo Rojo',
+    price: 3200,
+    stock: 2,
+    img: 'https://http2.mlstatic.com/D_NQ_NP_2X_724708-MLA51441220077_092022-F.webp'
+},
+{
+    id: 2,
+    name: 'Buzo Gris',
+    price: 4000,
+    stock: 4,
+    img: 'https://http2.mlstatic.com/D_NQ_NP_2X_787857-MLA51441263066_092022-F.webp'
+},
+{
+    id: 3,
+    name: 'Buzo Marron',
+    price: 3500,
+    stock: 6,
+    img: 'https://http2.mlstatic.com/D_NQ_NP_2X_724708-MLA51441220077_092022-F.webp'
+},
+{
+    id: 4,
+    name: 'Buzo Rosa',
+    price: 4000,
+    stock: 3,
+    img: 'https://http2.mlstatic.com/D_NQ_NP_2X_903891-MLA51441157496_092022-F.webp.'
+}];
  
-console.log(PRODUCTS)
+console.log(products)
 
-PRODUCTS.forEach((prod) => alert(`Los productos de CapHoodies son: ${prod.name}`))
+const divCards = document.querySelector(".divCards"); 
 
-let condition = 1;
+console.log(divCards)
 
-while(condition == 1){ 
+products.forEach((product) => {
 
-let userBuy = parseInt(prompt('Que producto desea comprar?  1- Buzo Verde $3200 2- Buzo Rojo $4000 3- Buzo Azul 3500 4- Buzo Blanco $5000'))
+    let div = document.createElement('div');
 
-switch(userBuy){
+    div.classList.add('card');
 
-    case 1:
-        if(product1.stock > 0 ){
+    div.innerHTML = `
 
-            CART.push(product1);
-            total += product1.price
-            product1.stock -= 1;
-            alert(`Compraste ${product1.name} quedó ${product1.stock} de stock`)
+    <img class="imgProd"src="${product.img}" alt="BuzoImg">
+    <br>
+    <h5>${product.name}</h5>
+    <p>PRECIO: $${product.price}</p>
+    <button class="btnAddCart" id=${product.id}>Agregar al carrito</button>
+    `
 
-        }
-        else{
-            alert("No hay stock")
-        }
-
-        break;
-    case 2:
-        if(product2.stock > 0 ){
-
-            CART.push(product2);
-            total += product2.price
-            product2.stock -= 1;
-            alert(`Compraste ${product2.name} quedó ${product2.stock} de stock`)
-        }
-        else{
-            alert("No hay stock")
-        }
-        break;
-    case 3:
-        if(product3.stock > 0 ){
-
-            CART.push(product3);
-            total += product3.price
-            product3.stock -= 1;
-            alert(`Compraste ${product3.name} quedó ${product3.stock} de stock`)
-        }
-        else{
-            alert("No hay stock")
-        }
-        break;
-    case 4:
-        if(product4.stock > 0 ){
-
-            CART.push(product4);
-            total += product4.price
-            product4.stock -= 1;
-            alert(`Compraste ${product4.name} quedó ${product4.stock} de stock`)
-        }
-        else{
-            alert("No hay stock")
-        }
-        break;
-    }
-
-
-    condition = prompt('Desea seguir comprando? 1- Si 2- No')
-
-    if(condition!=1){
-    alert(`Tu compra ha sido realizada, el total del monto es: $${CART.map(el => el.price).reduce(
-        (previousValue, currentValue) => previousValue + currentValue, 0)}`)
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// METODOS DE PRUEBA 
-
-// Suma de precios
-
-const totalPrecio = PRODUCTS.reduce((acc,el) => acc + el.price, 0);
-
-console.log(`El precio total de todos los buzos es: $${total} pesos`);
-
-// Mostras nombres de productos
-
-const namesProducts = PRODUCTS.map((el) => el.name);
-
-console.log(`El nombres de los buzos son: ${namesProducts}`);
-
-// Recorrer array con forEach
-
-let recorrido = PRODUCTS.forEach((el) => {
-
-    console.log(`el stock es de: ${el.stock}`)
+    divCards.append(div)
 
 })
 
-// Consulto si el nombre es estrictamente igual a buzo verde 
+let buttonsAddCart = document.querySelectorAll('.btnAddCart')
 
-let result = PRODUCTS.some((el) => el.name === 'Buzo Verde')
+console.log('button', buttonsAddCart)
 
-console.log(result)
+buttonsAddCart.forEach(button => {button.addEventListener('click', addCart)})
 
-// Metodo filter si el stock es mayor a 1
+function addCart(e) {
 
-if (PRODUCTS.filter((el)=> el.stock > 1).length > 0 ) {
+    let productOnCart = products.find(el => el.id === e.currentTarget.id);
 
-    console.log('Hay stock')
+    console.log('currentid',e.currentTarget.id)
+    
+    console.log(productOnCart)
+    CART.push(productOnCart);
 
-}else{
-
-    console.log('No hay stock')
-
-}
-
-// Ordeno productos por precio de menor a mayor 
+} 
 
 
-console.log(productsOrder)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let condition = 1;
+
+// while(condition == 1){ 
+
+// //let userBuy = parseInt(prompt('Que producto desea comprar?  1- Buzo Verde $3200 2- Buzo Rojo $4000 3- Buzo Azul 3500 4- Buzo Blanco $5000'))
+
+// switch(userBuy){
+
+//     case 1:
+//         if(product1.stock > 0 ){
+
+//             CART.push(product1);
+//             total += product1.price
+//             product1.stock -= 1;
+//             alert(`Compraste ${product1.name} quedó ${product1.stock} de stock`)
+
+//         }
+//         else{
+//             alert("No hay stock")
+//         }
+
+//         break;
+//     case 2:
+//         if(product2.stock > 0 ){
+
+//             CART.push(product2);
+//             total += product2.price
+//             product2.stock -= 1;
+//             alert(`Compraste ${product2.name} quedó ${product2.stock} de stock`)
+//         }
+//         else{
+//             alert("No hay stock")
+//         }
+//         break;
+//     case 3:
+//         if(product3.stock > 0 ){
+
+//             CART.push(product3);
+//             total += product3.price
+//             product3.stock -= 1;
+//             alert(`Compraste ${product3.name} quedó ${product3.stock} de stock`)
+//         }
+//         else{
+//             alert("No hay stock")
+//         }
+//         break;
+//     case 4:
+//         if(product4.stock > 0 ){
+
+//             CART.push(product4);
+//             total += product4.price
+//             product4.stock -= 1;
+//             alert(`Compraste ${product4.name} quedó ${product4.stock} de stock`)
+//         }
+//         else{
+//             alert("No hay stock")
+//         }
+//         break;
+//     }
+
+
+//    // condition = prompt('Desea seguir comprando? 1- Si 2- No')
+
+//     if(condition!=1){
+//     alert(`Tu compra ha sido realizada, el total del monto es: $${CART.map(el => el.price).reduce(
+//         (previousValue, currentValue) => previousValue + currentValue, 0)}`)
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // METODOS DE PRUEBA 
+
+// // Suma de precios
+
+// const totalPrecio = PRODUCTS.reduce((acc,el) => acc + el.price, 0);
+
+// console.log(`El precio total de todos los buzos es: $${total} pesos`);
+
+// // Mostras nombres de productos
+
+// const namesProducts = PRODUCTS.map((el) => el.name);
+
+// console.log(`El nombres de los buzos son: ${namesProducts}`);
+
+// // Recorrer array con forEach
+
+// let recorrido = PRODUCTS.forEach((el) => {
+
+//     console.log(`el stock es de: ${el.stock}`)
+
+// })
+
+// // Consulto si el nombre es estrictamente igual a buzo verde 
+
+// let result = PRODUCTS.some((el) => el.name === 'Buzo Verde')
+
+// console.log(result)
+// }
