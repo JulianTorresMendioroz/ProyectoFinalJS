1bfca526d16048525f77b85083f8419a88fa9658
JulianTorresMendioroz <juliantorresmendioroz348@gmail.com>
2022-11-23 21:07:18 -0300

    agrego archivos nuevos

--- index.js
+++ index.js
@@ -2,22 +2,6 @@ let CART = [];
 
 console.log('CART:',CART)
 
-// function newUserOn() { 
-
-// let newUser = prompt('Hola, ingrese su nuevo usuario')
-
-// if((newUser == '') || (newUser == ' ')) {
-
-//     console.log('Debe ingresar un usuario para poder continuar')
-
-// }else{
-
-//     alert(`Bienvenido a CapHoodies ${newUser}`)
-
-// }}
-
-// //newUserOn();
-
 const products = [
     
 {
@@ -55,6 +39,8 @@ const divCards = document.querySelector(".divCards");
 
 console.log('divcards', divCards)
 
+// Ver productos en home 
+
 products.forEach((product) => {
 
     let div = document.createElement('div');
@@ -78,314 +64,134 @@ products.forEach((product) => {
 
     div.append(buttonsAddCart)
     
+    // Push al carrito
+
     buttonsAddCart.addEventListener('click', () => {
 
-            CART.push({
-                id: product.id,
-                name: product.name,
-                price: product.price,
-                stock: product.stock,
-                img: product.img
+            CART.push(product)
 
-            })
-    })
+    let divCart = document.getElementById('divCart')
 
-    
+    divCart.classList.add('divCards')
 
-})
-
-let buttonCart = document.querySelector('.seeCartButton')
-
-console.log('button',buttonCart)
-
-let divCart = document.getElementById('divCart')
-
-divCart.classList.add('divCards')
-
-divCart.innerHTML = "";
-
-buttonCart.addEventListener('click', ()=> {
+    divCart.innerHTML = "";
 
     CART.forEach((product) => {
 
-      let divSeeProdCart = document.createElement('div')
+    let divSeeProdCart = document.createElement('div')
 
-      console.log('DIVPRODS', divSeeProdCart)
+    console.log('DIVPRODS', divSeeProdCart)
 
-      divSeeProdCart.classList.add('card')
+    divSeeProdCart.classList.add('card')
 
-        divSeeProdCart.innerHTML = `
+      divSeeProdCart.innerHTML = `
 
-        <img class="imgProd"src="${product.img}" alt="BuzoImg">
-        <br>
-        <h5>${product.name}</h5>
-        <p>PRECIO: $${product.price}</p>
-        <button class="buttonDelete">ELIMINAR ARTICULO</button>`;
+      <img class="imgProd"src="${product.img}" alt="BuzoImg">
+      <br>
+      <h5>${product.name}</h5>
+      <p>PRECIO: $${product.price}</p>
+      <button class="buttonDelete">ELIMINAR ARTICULO</button>`;
+      
+
+          divCart.append(divSeeProdCart) 
+
+
         
+          let buttonDelete = divSeeProdCart.querySelector('.buttonDelete')
 
-            divCart.append(divSeeProdCart) 
+          buttonDelete.addEventListener('click', () => {
+     
+            deleteId(product.id)
 
+              
+          }) 
+     })
 
-            let buttonDelete = divSeeProdCart.querySelector('.buttonDelete')
+     // Suma precio carrito
 
-            buttonDelete.addEventListener('click', () => {
+     totalCart()
 
-                deleteId(product.id)
-            })
-
-
-            // buttonDelete.forEach(button => {button.addEventListener('click', deleteArticle)})
-         
-            // function deleteArticle() {
-         
-            //      deleteId(product.id)
-         
-         
-         
-            // }
-         
-    
-   })
-
-  
-
-
-   let totalCartDiv = document.querySelector('.divTotal')    
-
-   let subDivTotal = document.createElement('div')
-   
-   subDivTotal.classList.add('subDivTotal')
-
-   totalOnCart = CART.reduce((acc, el) => acc + el.price, 0)
-
-   console.log('TOTALCARRITO',totalOnCart)
-
-   subDivTotal.innerHTML = `
-   
-       <h3 class="totalOnCart">SU TOTAL DE LA COMPRA ES: $${totalOnCart}</h3>
-   
-    `;
-
-    totalCartDiv.append(subDivTotal)
 
 })
 
+})
 
-const deleteId = (id) => {
+// Actualizar carrito 
 
-    let idProd = CART.find((el) => el.id === id)
+function updatedCart () {
 
-    CART = CART.filter((cartId) => {
+    
+    divCart.innerHTML = "";
 
-        return cartId != idProd
+    CART.forEach((product) => {
+    
+        let divSeeProdCart = document.createElement('div')
+    
+        console.log('DIVPRODS', divSeeProdCart)
+    
+        divSeeProdCart.classList.add('card')
+    
+          divSeeProdCart.innerHTML = `
+    
+          <img class="imgProd"src="${product.img}" alt="BuzoImg">
+          <br>
+          <h5>${product.name}</h5>
+          <p>PRECIO: $${product.price}</p>
+          <button class="buttonDelete">ELIMINAR ARTICULO</button>`;
+          
+    
+              divCart.append(divSeeProdCart) 
 
+              let buttonDelete = divSeeProdCart.querySelector('.buttonDelete')
+
+          buttonDelete.addEventListener('click', () => {
+
+            
+     
+              deleteId(product.id)
+
+              totalCart()
+
+
+          })
+
+
+      
+
+        
     })
-    console.log('ID cart',idProd)
+
 }
 
 
 
+const deleteId = (id) => {
 
+    let indexProd = CART.findIndex(prod => prod.id === id);
+    
+	CART.splice(indexProd, 1);
 
+    updatedCart()
 
+}
 
 
+    const totalCart = () => {
 
+        let totalCartDiv = document.querySelector('.divTotal')    
+    
+        let subDivTotal = document.createElement('div')
+    
+        subDivTotal.classList.add('subDivTotal')
+        totalOnCart = CART.reduce((acc, el) => acc + el.price, 0)
+        console.log('TOTALCARRITO',totalOnCart)
+        subDivTotal.innerHTML = `
+    
+        <h3 class="totalOnCart">SU TOTAL DE LA COMPRA ES: $${totalOnCart}</h3>
+    
+        `;
 
+        totalCartDiv.append(subDivTotal)
+    
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let divCart = document.querySelector('.divCart')
-
-// let buttonCart = document.createElement('button')
-
-// buttonCart.innerText = 'Ver carrito'
-
-// buttonCart.classList.add('buttonCart')
-
-// divCart.append(buttonCart)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let condition = 1;
-
-// while(condition == 1){ 
-
-// //let userBuy = parseInt(prompt('Que producto desea comprar?  1- Buzo Verde $3200 2- Buzo Rojo $4000 3- Buzo Azul 3500 4- Buzo Blanco $5000'))
-
-// switch(userBuy){
-
-//     case 1:
-//         if(product1.stock > 0 ){
-
-//             CART.push(product1);
-//             total += product1.price
-//             product1.stock -= 1;
-//             alert(`Compraste ${product1.name} quedó ${product1.stock} de stock`)
-
-//         }
-//         else{
-//             alert("No hay stock")
-//         }
-
-//         break;
-//     case 2:
-//         if(product2.stock > 0 ){
-
-//             CART.push(product2);
-//             total += product2.price
-//             product2.stock -= 1;
-//             alert(`Compraste ${product2.name} quedó ${product2.stock} de stock`)
-//         }
-//         else{
-//             alert("No hay stock")
-//         }
-//         break;
-//     case 3:
-//         if(product3.stock > 0 ){
-
-//             CART.push(product3);
-//             total += product3.price
-//             product3.stock -= 1;
-//             alert(`Compraste ${product3.name} quedó ${product3.stock} de stock`)
-//         }
-//         else{
-//             alert("No hay stock")
-//         }
-//         break;
-//     case 4:
-//         if(product4.stock > 0 ){
-
-//             CART.push(product4);
-//             total += product4.price
-//             product4.stock -= 1;
-//             alert(`Compraste ${product4.name} quedó ${product4.stock} de stock`)
-//         }
-//         else{
-//             alert("No hay stock")
-//         }
-//         break;
-//     }
-
-
-//    // condition = prompt('Desea seguir comprando? 1- Si 2- No')
-
-//     if(condition!=1){
-//     alert(`Tu compra ha sido realizada, el total del monto es: $${CART.map(el => el.price).reduce(
-//         (previousValue, currentValue) => previousValue + currentValue, 0)}`)
-//     }
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // METODOS DE PRUEBA 
-
-// // Suma de precios
-
-// const totalPrecio = PRODUCTS.reduce((acc,el) => acc + el.price, 0);
-
-// console.log(`El precio total de todos los buzos es: $${total} pesos`);
-
-// // Mostras nombres de productos
-
-// const namesProducts = PRODUCTS.map((el) => el.name);
-
-// console.log(`El nombres de los buzos son: ${namesProducts}`);
-
-// // Recorrer array con forEach
-
-// let recorrido = PRODUCTS.forEach((el) => {
-
-//     console.log(`el stock es de: ${el.stock}`)
-
-// })
-
-// // Consulto si el nombre es estrictamente igual a buzo verde 
-
-// let result = PRODUCTS.some((el) => el.name === 'Buzo Verde')
-
-// console.log(result)
-// }
